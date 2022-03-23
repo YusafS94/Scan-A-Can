f7601a798edada0fde2a28ac037a2dc2b859a20f
Yusaf <42899208+YusafS94@users.noreply.github.com>
2022-03-23 03:47:20 +0000

    Add files via upload

--- scanned/script.js
+++ scanned/script.js
@@ -9,10 +9,19 @@ let progress = setInterval(() => {
   progressValue++;
   valueContainer.textContent = `${progressValue}/150`;
   progressBar.style.background = `conic-gradient(
-      #4d5bf9 ${progressValue * 2 }deg,
-      #cadcff ${progressValue *3}deg
+      #4d5bf9 ${progressValue * 2}deg,
+      #cadcff ${progressValue * 3}deg
   )`;
   if (progressValue == progressEndValue) {
     clearInterval(progress);
   }
 }, speed);
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
